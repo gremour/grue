aa2d8d633b17596b47ae764b7b08047dd13b4620
gremour <razorgrem@gmail.com>
2019-10-14 16:43:04 +0300

    Added LineEdit text trimming and limit

--- lineedit.go
+++ lineedit.go
@@ -5,6 +5,7 @@ type LineEdit struct {
 	*Panel
 	CursorPos  int
 	TextOffset int
+	TextLimit  int
 
 	OnTextChanged     func()
 	OnEditingFinished func()
@@ -13,7 +14,8 @@ type LineEdit struct {
 // NewLineEdit creates new line edit.
 func NewLineEdit(parent Widget, b Base) *LineEdit {
 	le := &LineEdit{
-		Panel: NewPanel(nil, b),
+		Panel:     NewPanel(nil, b),
+		TextLimit: 1000,
 	}
 	InitWidget(parent, le)
 
@@ -34,24 +36,14 @@ func (le *LineEdit) Paint() {
 	r := le.GlobalRect()
 	theme := le.MyTheme()
 	tcur, _ := theme.Drawers[ThemeLineEdit]
-	tcol := theme.TextColor
-	txt := le.Text
-	if le.Text == "" {
-		tcol = theme.PlaceholderColor
-		txt = le.PlaceholderText
-	}
 	tcur.Draw(le.Surface, r)
 	editMode := le.Equals(le.Surface.Focus())
 	if editMode {
-		curRight := theme.Pad
-		if le.Text != "" && le.CursorPos >= le.TextOffset {
-			cursorAfterStr := le.Text[le.TextOffset : le.CursorPos-le.TextOffset]
-			curRight += le.Surface.GetTextRect(cursorAfterStr, theme.TitleFont).W()
-		}
-		le.DrawImageAndText("", le.Text, tcol, 0, AlignLeft)
+		curRight := le.cursorPos()
+		le.drawTextTrimmed()
 		theme.CursorDrawer.Draw(le.Surface, r.Min.Add(V(curRight, theme.Pad)), le.Rect.H()-theme.Pad*2)
 	} else {
-		le.DrawImageAndText("", txt, tcol, 0, AlignLeft)
+		le.drawTextTrimmed()
 	}
 }
 
@@ -62,6 +54,7 @@ func (le *LineEdit) onKeys() bool {
 			le.OnEditingFinished()
 		}
 		le.Surface.SetFocus(nil)
+		le.TextOffset = 0
 	case le.Surface.JustPressed(KeyBackspace) || le.Surface.Repeated(KeyBackspace):
 		if le.CursorPos == 0 {
 			break
@@ -100,6 +93,9 @@ func (le *LineEdit) onKeys() bool {
 	case le.Surface.JustPressed(KeyEnd):
 		le.CursorPos = len(le.Text)
 	default:
+		if len(le.Text) >= le.TextLimit {
+			break
+		}
 		t := le.Surface.KeysInput()
 		if len(t) > 0 {
 			le.Text = le.Text[:le.CursorPos] + t + le.Text[le.CursorPos:len(le.Text)]
@@ -109,5 +105,73 @@ func (le *LineEdit) onKeys() bool {
 			le.OnTextChanged()
 		}
 	}
+	le.updateTextOffest()
 	return true
+}
+
+// Return the horizontal position of cursor with current TextOffset
+func (le *LineEdit) cursorPos() float64 {
+	curRight := le.MyTheme().Pad
+	if le.Text == "" || le.CursorPos == le.TextOffset {
+		return curRight
+	}
+	if le.CursorPos > le.TextOffset {
+		textPiece := le.Text[le.TextOffset:le.CursorPos]
+		curRight += le.Surface.GetTextRect(textPiece, le.MyTheme().TitleFont).W()
+	} else {
+		if le.TextOffset > len(le.Text) {
+			le.TextOffset = len(le.Text)
+		}
+		textPiece := le.Text[le.CursorPos:le.TextOffset]
+		curRight -= le.Surface.GetTextRect(textPiece, le.MyTheme().TitleFont).W()
+	}
+	return curRight
+}
+
+// Update TextOffset so cursor is shown.
+func (le *LineEdit) updateTextOffest() {
+	pad := le.MyTheme().Pad
+	curPos := le.cursorPos()
+	for curPos < le.Rect.W()/2+pad && le.TextOffset > 0 {
+		le.TextOffset--
+		curPos = le.cursorPos()
+	}
+	for curPos >= le.Rect.W()-pad && le.TextOffset <= len(le.Text) {
+		le.TextOffset++
+		curPos = le.cursorPos()
+	}
+}
+
+func (le *LineEdit) drawTextTrimmed() {
+	theme := le.MyTheme()
+
+	tcol := theme.TextColor
+	text := le.Text
+	offs := le.TextOffset
+	if le.Text == "" {
+		tcol = theme.PlaceholderColor
+		text = le.PlaceholderText
+		offs = 0
+	}
+
+	minSymWidth := le.Surface.GetTextRect(" ", theme.TitleFont).W()
+	if minSymWidth == 0 {
+		minSymWidth = 1
+	}
+	maxW := (le.Rect.W() - theme.Pad*2)
+	maxSymCount := int(maxW/minSymWidth) + 1
+	lastSym := le.TextOffset + maxSymCount
+	if lastSym > len(le.Text) {
+		lastSym = len(le.Text)
+	}
+	var txt string
+	for {
+		txt = text[offs:lastSym]
+		if le.Surface.GetTextRect(txt, theme.TitleFont).W() <= maxW ||
+			lastSym <= offs {
+			break
+		}
+		lastSym--
+	}
+	le.DrawImageAndText("", txt, tcol, 0, AlignLeft)
 }
